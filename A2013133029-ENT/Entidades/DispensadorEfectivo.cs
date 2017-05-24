fa8c169c067ed6c6fc4962a3194557cb7fae4d76
A2013133029 <alvin_diaz@usmp.pe>
2017-05-23 23:59:34 -0500

    PORFA

---
+++ A2013133029-ENT/Entidades/DispensadorEfectivo.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace A2013133029_ENT
+{
+    class DispensadorEfectivo
+    {
+        public int DispensadorEfectivoId { get; set; }
+        private int _Contador = -500;
+        public ATM ATM { get; set; }
+        public Retiro Retiro { get; set; }
+
+        public int Contador
+        {
+            get { return _Contador; }
+        }
+
+        public DispensadorEfectivo()
+        {
+
+        }
+    }
+}
